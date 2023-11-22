5b446be9325f851f0d45a7b51a338545205c1cd9
NikitaGurba <150122791+NikitaGurba@users.noreply.github.com>
2023-11-22 20:12:40 +0300

    Update Review.vue

--- src/components/Review.vue
+++ src/components/Review.vue
@@ -1,10 +1,70 @@
 <script setup>
+import { ref } from "vue";
 import Grade from "@/components/Grade.vue";
+import { io } from "socket.io-client";
 const props = defineProps({
   data: Object,
 });
+const socket = io(
+  process.env.VUE_APP_SOCKET_URL || import.meta.env.VITE_SOCKET_URL,
+);
 const { name, grade, text, lecturer } = props.data;
+const likes = ref(props.data.likes);
+const dislikes = ref(props.data.dislikes);
+const liked = ref(false);
+const disliked = ref(false);
+  
+const checkRate = () => {
+  socket.emit("getIp");
+  socket.on("takeIp", (ip) => {
+    console.log(ip);
+    likes.value.map((item) => {
+      if (item === ip) {
+        liked.value = true;
+      }
+    });
+    dislikes.value.map((item) => {
+      if (item === ip) {
+        disliked.value = true;
+      }
+    });
+  });
+};
+checkRate();
 const date = new Date(props.data.date).toLocaleDateString("ru-RU");
+  
+const like = () => {
+  socket.emit("like", _id);
+  socket.on("takeLikes", (likesList) => {
+    likes.value = likesList;
+    liked.value = !liked.value;
+    socket.off();
+    if (disliked.value) {
+      socket.emit("dislike", _id);
+      socket.on("takeDislikes", (dislikeList) => {
+        dislikes.value = dislikeList;
+        disliked.value = !disliked.value;
+        socket.off();
+      });
+    }
+  });
+};
+const dislike = () => {
+  socket.emit("dislike", _id);
+  socket.on("takeDislikes", (dislikeList) => {
+    dislikes.value = dislikeList;
+    disliked.value = !disliked.value;
+    socket.off();
+    if (liked.value) {
+      socket.emit("like", _id);
+      socket.on("takeLikes", (likesList) => {
+        likes.value = likesList;
+        liked.value = !liked.value;
+        socket.off();
+      });
+    }
+  });
+};
 </script>
 
 <template>
@@ -29,6 +89,29 @@ const date = new Date(props.data.date).toLocaleDateString("ru-RU");
         </div>
       </div>
       <span class="content__text">{{ text }}</span>
+      <div class="rate">
+        <div class="rate__cont">
+          <button
+            class="rate__button rate__like"
+            @click="like"
+            :style="{ background: liked ? '#ff5555' : '#1e2029' }"
+          >
+            <font-awesome-icon icon="thumbs-up" class="fa-1x" />
+          </button>
+          <span>{{ likes.length }}</span>
+        </div>
+        <span class="rate__sep">|</span>
+        <div class="rate__cont">
+          <button
+            class="rate__button rate__dislike"
+            @click="dislike"
+            :style="{ background: disliked ? '#ff5555' : '#1e2029' }"
+          >
+            <font-awesome-icon icon="thumbs-down" class="fa-1x" />
+          </button>
+          <span>{{ dislikes.length }}</span>
+        </div>
+      </div>
     </main>
   </article>
 </template>
@@ -42,6 +125,30 @@ const date = new Date(props.data.date).toLocaleDateString("ru-RU");
 .content {
   display: flex;
   flex-direction: column;
+  gap: 0.3rem;
+}
+.rate__button {
+  padding: 0.5rem;
+  border-radius: 2rem;
+  border: 0.1rem solid #9ea2b9;
+  border: none;
+  width: 2.2rem;
+  height: 2.2rem;
+  background-color: #1e2029;
+  cursor: pointer;
+}
+.rate {
+  margin-top: 0.5rem;
+  display: flex;
+  gap: 0.5rem;
+  align-items: center;
+}
+.rate__sep {
+  color: #4b4e60;
+}
+.rate__cont {
+  display: flex;
+  align-items: center;
   gap: 0.3rem;
 }
 .content-info {
